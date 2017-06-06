8757fcb4aaeef825ad1cf7cfe9d6eac96ea51c04
tylerh <tylerh@qualtrics.com>
2017-06-06 13:36:54 -0600

    changing the general styles for the application, added a favicon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // var express = require('express');
 // var path = require('path');
-// var favicon = require('serve-favicon');
 // var logger = require('morgan');
 // var cookieParser = require('cookie-parser');
 // var bodyParser = require('body-parser');
@@ -15,6 +14,7 @@ var hash = require('bcrypt-nodejs');
 var path = require('path');
 var passport = require('passport');
 var localStrategy = require('passport-local' ).Strategy;
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,8 +28,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
